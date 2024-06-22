0d9b5d084a5e68f60cbbf6a1eb0c823e78b29b23
ThisIsTeam <amteamxmail@gmail.com>
2024-06-22 17:10:17 +0700

    update timeline

--- data/timeline.js
+++ data/timeline.js
@@ -1,22 +1,23 @@
 export const timeline = [
     {
         year:"2024",
-        imgSrc:"",
+        imgSrc:"https://karpathy.ai/assets/tesla_logo2.jpg",
         content: "test",
     },
     {
         year:"2022-2023",
-        imgSrc:"",
+        imgSrc:"https://karpathy.ai/assets/openai_logo.png",
         content:"test2"
     },
     {
         year:"2022-2023",
-        imgSrc:"",
+        imgSrc:"https://karpathy.ai/assets/tesla_logo2.jpg",
         content:"akara test, how to put link here?"
     },
-    {
+     {
         year:"2022-2023",
-        imgSrc:"",
-        content:`you can put link like <a>this</a> by using tag a with src`
+        imgSrc:"https://karpathy.ai/assets/tesla_logo2.jpg",
+        content:"akara test, how to put link here?fsdfsefsfsfwfwfedfdgdddgdgrdgdrgdgdrgdgdgg"
     },
+
 ]
--- scripts/displayTimeline.js
+++ scripts/displayTimeline.js
@@ -4,15 +4,25 @@ const display = document.getElementById("displaytimeline");
 
 export default function DisplayTimeline() {
     let displayData = timeline.map((object) => {
-        const year = object.year;
+        let year = object.year;
         let content = object.content;
+        let img = object.imgSrc;
 
         content = content.replace(/<a\b[^>]*>(.*?)<\/a>/g, '<a href="/test" style="color: blue; text-decoration: underline;">$1</a>');
 
         return `
-        <div class="">
-            <h5>year: ${year}</h5>
-            <p>${content}</p>
+        <div class="flex text-start h-auto">
+            <div class="line flex justify-end w-1/5 h-max border-r-2"> 
+                <p class="timespace text-xs text-right md:text-base">${year}</p>
+                <div class="timelinedot"></div>
+            </div>
+            <div class="w-auto px-2 pb-1">
+                <img src=${img} style="max-width:80px;border-radius:10px"></img>
+            </div>
+            <div class="w-3/6 pb-2">
+                <p class="text-sm break-all md:text-lg">${content}</p>
+            </div>
+               
         </div>
         `;
     }).join("");
