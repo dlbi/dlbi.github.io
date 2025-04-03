3447f3acc127a8b573eff554c0763a734f3fba9c
ACitronella <phuriwat.13042@gmail.com>
2025-04-03 17:42:42 +0700

    fix footer

--- scripts/fixedPageElems.js
+++ scripts/fixedPageElems.js
@@ -13,11 +13,33 @@ function displayNavbar(){
     `;
 }
 
-const footer = "Akara Supratak | akara.sup@mahidol.ac.th | Faculty of Information and Communication Technology, Mahidol University, 999 Phuttamonthon 4 Road, Salaya , Nakhon Pathom, 73170 THAILAND";
-const display = document.getElementById("footer");
-
+const footer_elem = document.getElementById("footer");
 function displayFooter() {
-    display.innerHTML = `<div class="text-center">${footer}</div>`;
+    footer_elem.innerHTML = `
+    
+        <div class="grid grid-row-5"></div>
+            <div class="grid grid-cols-7 gap-4">
+                <div class="col-start-1"><div class="text-right"><b>Location</b></div></div>
+                <div class="col-start-2 col-span-2"><div class="text-left">Faculty of Information and Communication Technology</div></div>
+
+                <div class="col-start-5"><div class="text-right"><b>Contact Person</b></div></div>
+                <div class="col-start-6 col-span-2"><div class="text-left">Akara Supratak, Ph.D.</div></div>
+            </div>
+            <div class="grid grid-cols-7 gap-4">
+                <div class="col-start-2 col-span-2"><div class="text-left">Mahidol University</div></div>
+                <div class="col-start-6 col-span-2"><div class="text-left">akara.sup@mahidol.ac.th</div></div>
+            </div>
+            <div class="grid grid-cols-7 gap-4">
+                <div class="col-start-2 col-span-3"><div class="text-left">999 Phuttamonthon 4 Road, Salaya</div></div>
+            </div>
+            <div class="grid grid-cols-7 gap-4">
+                <div class="col-start-2 col-span-3"><div class="text-left">Nakhon Pathom, 73170 THAILAND</div></div>
+            </div>
+            <div class="grid grid-cols-7 gap-4">
+            </div>
+        </div>
+    
+    `;
 }
 
 displayNavbar();
